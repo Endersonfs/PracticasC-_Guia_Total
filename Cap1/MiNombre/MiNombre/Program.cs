146b1dae3287e4995f413d1d4ce94f49661f0e41
Enderson Florian Solano <efloriansolano@gmail.com>
2018-08-16 03:43:44 +0200

    Cap1

---
+++ Cap1/MiNombre/MiNombre/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiNombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi nombre es Enderson Florian Solano");
+            //este es el Error aproposito
+            //consola.writeLine('Soy Soto');
+            Console.WriteLine("Carrera: Tecnologo en Software (ITLA)");
+            Console.WriteLine("Corre Electronico: efloriansolano@gmail.com");
+        }
+    }
+}
