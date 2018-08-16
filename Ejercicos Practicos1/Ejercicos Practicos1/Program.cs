7521cc9535eefad455e2e1cbf5ddb3ebbeab9196
Raul Sanchez Reyes <raulsanchez23s@outlook.com>
2018-08-16 15:57:40 -0400

    JJ

---
+++ Ejercicos Practicos1/Ejercicos Practicos1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicos_Practicos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Raul Sanchez");
+            Console.ReadKey();
+            //Error a proposito
+            //Consol.writeline(raul);
+
+        }
+    }
+}
