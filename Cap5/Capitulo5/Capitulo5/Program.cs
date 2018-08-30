8031d561e2f2baadfa4f946b8976a46afb7815f7
Enderson Florian Solano <efloriansolano@gmail.com>
2018-08-30 06:23:54 +0200

    Cap5 END

---
+++ Cap5/Capitulo5/Capitulo5/Program.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capitulo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables necesarias
+            int numero;
+            //capturando datos
+            Console.WriteLine("1. Transformar grados a Radianes");
+            Console.WriteLine("2. Transformar grados centígrados a grados Fahrenheit");
+            Console.WriteLine("3. Transfomrar perímetro de polígono regular");
+            Console.WriteLine("4. Calcular el factorial de un número.");
+            Console.WriteLine("5. Transformar de Numero a Letras");
+            numero = Convert.ToInt32(Console.ReadLine());
+            //comparando
+            if (numero == 1)
+            {
+                //grados a radianes
+                GradosRadianes();
+            }
+            else if (numero == 2)
+            {
+                //grados centigrados a fahrenheit
+                CentigradosFahrenheit();
+            }
+            else if (numero == 3)
+            {
+                //perimetro de pologono regular
+                PerimetroPoligonoRegular();
+            }
+            else if (numero == 4)
+            {
+                //factorial de un numero
+                Factorial();
+            }
+            else if (numero == 5)
+            {
+                //numero a letras
+                NumeroLetra();
+            }
+            else
+            {
+                Console.WriteLine("Introdusca un valor que este en el intervalo");
+            }
+
+        }//final de metodo main
+        static float PedirValor()
+        {
+            //variables necesaria
+            float resultado = 0.0f;
+            Console.WriteLine("Introdusca Su Valor: ");
+            resultado = Convert.ToSingle(Console.ReadLine());
+            return resultado;
+        }//final de metodo pedir valor
+        static void DarResultado(float valor)
+        {
+            Console.WriteLine("Su Resultado es: {0}", valor);
+        }//final del metodo resultado
+        static void GradosRadianes()
+        {
+            //variables necesarias
+            float grados, resultado;
+            grados = PedirValor();
+            resultado = Convert.ToSingle((grados / 180) * 3.1416);
+            DarResultado(resultado);
+        }//final de la funcion grados a radianes
+        static void CentigradosFahrenheit()
+        {
+            //variables necesarias
+            float grado = 0.0f;
+            float a, b, c, resultado;
+            grado = PedirValor();
+            //formulas para convertir
+            a = grado * 2;
+            b = a * 0.1f;
+            c = a - b;
+            resultado = c + 32;
+            DarResultado(resultado);
+        }//final de la funcion centigrados a fahrenheit
+        static void PerimetroPoligonoRegular()
+        {
+            //variables necesarias
+            float lados, Longitud, resultado;
+            Console.WriteLine("Dame la cantidad de lados");
+            lados = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Dame la cantidad de uno de sus lados");
+            Longitud = Convert.ToSingle(Console.ReadLine());
+            //formula
+            resultado = lados * Longitud;
+            DarResultado(resultado);
+        }//final de la funcion perimeto 
+        static void Factorial()
+        {
+            //varibles necesarias
+            float numero;
+            float resultado;
+            float suma = 1f;
+            //pedir valor
+            numero = PedirValor();
+            for (int i = 1; i <= numero; i++)
+            {
+                resultado = numero * Convert.ToSingle(i);
+                suma *= i;
+                Console.WriteLine("{0}x{1}={2}", numero, i, resultado);
+
+            }
+            Console.WriteLine("Su Factorial es: {0}", suma);
+        }//final de la funcion factorial
+        static void NumeroLetra()
+        {
+            //variables necesarias
+            string a="";
+            string resultado;
+            a = Convert.ToString(PedirValor());
+            String ab= Convert.ToString(a);
+            if (ab.Length == 1)
+            {
+                resultado = Letras(a.Length);
+                Console.WriteLine("Su resultado es, {0}, la cantidad de letras es: {1}", resultado, ab.Length);
+            }
+            else if (ab.Length == 2)
+            {
+                //variables necesarias
+                string unidad = "";
+                string decena = "";
+                //calculo de tipo de variable
+                unidad = Letras(Convert.ToSingle(a.Substring(0, 1) + 0));
+                decena = Letras(Convert.ToSingle(a.Substring(1, 1)));
+                Console.WriteLine("Su resultado es, {0} {1}", unidad, decena);
+            }
+            else if (ab.Length == 3)
+            {
+                //variables necesarias
+                string unidad = "";
+                string decena = "";
+                string centena = "";
+                //calculo de tipo de variable
+                unidad = Letras(Convert.ToSingle(a.Substring(0, 1) + 0 + 0));
+                decena = Letras(Convert.ToSingle(a.Substring(1, 1) + 0));
+                centena = Letras(Convert.ToSingle(a.Substring(2, 1)));
+                Console.WriteLine("Su resulado es, {0} {1} {2}", unidad, decena, centena);
+            }
+            else
+            {
+                Console.WriteLine("Solo puedo Leer los numeros hasta 999");
+            }
+        }//numeros letras
+        static string Letras(float valor)
+        {
+            string resultado="";
+            switch (valor)
+            {
+                case 1:
+                    resultado = "Uno";
+                    break;
+                case 2:
+                    resultado = "Dos";
+                    break;
+                case 3:
+                    resultado = "Tres";
+                    break;
+                case 4:
+                    resultado = "Cuatro";
+                    break;
+                case 5:
+                    resultado = "Cinco";
+                    break;
+                case 6:
+                    resultado = "Seis";
+                    break;
+                case 7:
+                    resultado = "Siete";
+                    break;
+                case 8:
+                    resultado = "Ocho";
+                    break;
+                case 9:
+                    resultado = "Nueve";
+                    break;
+                case 10:
+                    resultado = "Diez";
+                    break;
+                case 20:
+                    resultado = "Veinte";
+                    break;
+                case 30:
+                    resultado = "Treinta";
+                    break;
+                case 40:
+                    resultado = "Cuarenta";
+                    break;
+                case 50:
+                    resultado = "Cincuenta";
+                    break;
+                case 60:
+                    resultado = "Sesenta";
+                    break;
+                case 70:
+                    resultado = "Setenta";
+                    break;
+                case 80:
+                    resultado = "Ochenta";
+                    break;
+                case 90:
+                    resultado = "Noventa";
+                    break;
+                case 100:
+                    resultado = "Cien";
+                    break;
+                case 200:
+                    resultado = "Doscientos";
+                    break;
+                case 300:
+                    resultado = "Tresciento";
+                    break;
+                case 400:
+                    resultado = "Cuatrocien";
+                    break;
+                case 500:
+                    resultado = "Quientos";
+                    break;
+                case 600:
+                    resultado = "Seisciento";
+                    break;
+                case 700:
+                    resultado = "Setecientos";
+                    break;
+                case 800:
+                    resultado = "Ochocientos";
+                    break;
+                case 900:
+                    resultado = "Noveciento";
+                    break;
+                case 1000:
+                    resultado = "Mil";
+                    break;
+
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Cap5/program/program/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            int numero = 0;
+            //capturando datos
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Multiplicion");
+            Console.WriteLine("4. Division");
+            Console.Write("Escribe la opcion que desea: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            if (numero == 1)
+            {
+                //suma
+                suma();
+            }
+            else if (numero == 2)
+            {
+                //variable para resultado
+                float resultado = 0;
+                //asignando valor de funcion a la variable
+                resultado=resta();
+                //imprimiendo el valor
+                Console.WriteLine("El resultado es: {0}",resultado);
+            }
+            else if (numero == 3)
+            {
+                //multiplicacion
+                //variables necesarias
+                float a = 0;
+                float b = 0;
+                /* 
+                string Numero = "";
+                //capturando datos
+                Console.WriteLine("Dame el primer dato");
+                Numero = Console.ReadLine();
+                a = Convert.ToSingle(Numero);
+                /*Console.WriteLine("Dame el segundo dato");
+                Numero = Console.ReadLine();
+                b = Convert.ToSingle(Numero);
+                //imprimiendo resultado
+                */
+
+                //nueva forma utilizando optimizacion
+                a = pideFlotante("Dame el primer numero");
+                b = pideFlotante("Dame el segundo numero");
+                multiplicar(a,b);
+            }
+            else if (numero == 4)
+            {
+                //division
+                //variables necesarias
+                float a = 0, b = 0;
+                float resulstado;
+                //Capturando datos
+                //nueva forma optimizacion de datos
+                a = pideFlotante("Dame el primer dato");
+                b = pideFlotante("Dame el segundo dato");
+                //invocamos a la funcion
+                resulstado = division(a, b);
+                //imprimeindo resultado
+                Console.WriteLine("El Resultado es: {0}",resulstado);
+            }
+            else
+            {
+                //no es una opcion
+                Console.WriteLine("Digite una Opcion Correcta");
+            }//final del if
+        }//final del metodo main
+        static void suma()
+        {
+            //variables necesarias
+            float a = 0, b = 0, r = 0;
+            //pedimos los valores
+            a = pideFlotante("Dame el primer dato");
+            b = pideFlotante("Dame el segundo dato");
+
+            //calculamos el resultado
+            r = a + b;
+            //imprimiendo resultado
+            Console.WriteLine("El Resultado es: {0}", r);
+        }//metodo suma
+        static float resta()
+        {
+            //variables necesarias
+            float a = 0, b = 0, r = 0;
+            //pedimos los valores
+            a = pideFlotante("Dame el primer dato");
+            b = pideFlotante("Dame el segundo dato");
+            //calculamos el resultado
+            r = a - b;
+            //retornamos el resultado
+            return r;
+        }//final del metodo resta
+        static void multiplicar(float a,float b)
+        {
+            //variables necesarias
+            float r = 0;
+            //calculamos el resultado
+            r = a * b;
+            //mostramos el resultado
+            Console.WriteLine("El resultado es: {0}",r);
+        }//final del metodo multiplicar
+        static float division(float a, float b)
+        {
+            //variables necesarias
+            float r = 0;
+            if (b == 0)
+            {
+                Console.WriteLine("No es Posible Dividir en 0 (Infinito)");
+                return 0.0f;
+            }
+            else
+            {
+                r = a / b;
+                return r;
+            }
+        }//final del metodo division
+        static float pideFlotante(string mensaje)
+        {
+            //variables necesarios
+            float numero = 0.0f;
+            string valor = "";
+            //mostramos el mensaje 
+            Console.WriteLine(mensaje);
+            //obtenemos el valor
+            valor = Console.ReadLine();
+            numero = Convert.ToSingle(valor);
+            //regresamos el dato
+            return numero;
+        }
+    }
+}
